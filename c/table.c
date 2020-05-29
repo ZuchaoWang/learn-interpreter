2a5b279744ce54f6b1b670078cbdf565d1b92ac7
zuchao.wang <zuchao.wang@gmail.com>
2020-05-29 13:27:30 +0800

    half way of chap30

--- c/table.c
+++ c/table.c
@@ -21,7 +21,7 @@ void freeTable(Table* table) {
 
 static Entry* findEntry(Entry* entries, int capacity,
                         ObjString* key) {            
-  uint32_t index = key->hash % capacity;
+  uint32_t index = key->hash & (capacity - 1);
   Entry* tombstone = NULL;              
 
   for (;;) {                                         
@@ -40,7 +40,7 @@ static Entry* findEntry(Entry* entries, int capacity,
       return entry;                                  
     }                                                 
 
-    index = (index + 1) % capacity;                  
+    index = (index + 1) & (capacity - 1);                  
   }                                                  
 }
 
@@ -125,7 +125,7 @@ ObjString* tableFindString(Table* table, const char* chars, int length,
   // but in chunk.constants, they are still represented as two entries, just pointing to the same sObjString
   if (table->count == 0) return NULL;
 
-  uint32_t index = hash % table->capacity;                             
+  uint32_t index = hash & (table->capacity - 1);                             
 
   for (;;) {                                                           
     Entry* entry = &table->entries[index];                             
@@ -140,7 +140,7 @@ ObjString* tableFindString(Table* table, const char* chars, int length,
       return entry->key;                                               
     }                                                                  
 
-    index = (index + 1) % table->capacity;                             
+    index = (index + 1) & (table->capacity - 1);                             
   }                                                                    
 }
 
